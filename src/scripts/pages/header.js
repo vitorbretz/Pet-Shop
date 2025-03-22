b43ec0676f138464202a1d92d5b5ad600483c499
vitorbretz <vitorbretz28@gmail.com>
2025-03-22 14:39:09 -0300

    teste git em terminal

--- src/scripts/pages/header.js
+++ src/scripts/pages/header.js
@@ -1,16 +1,9 @@
 export default function menuShow(){
-const btnBurguer = document.querySelector('.btn-burguer')
+const btnBurguer = document.querySelector('.btn-burguer');
+const menu = document.querySelector('#nav');
 btnBurguer.addEventListener('click', ToggleEvent);
 function ToggleEvent(){
-  btnBurguer.classList.toggle('active-menu')
+  menu.classList.toggle('active-menu')
 }  
-document.addEventListener("DOMContentLoaded", function () {
-  const items = document.querySelectorAll(".item-menu");
 
-  items.forEach((item, index) => {
-      setTimeout(() => {
-          item.classList.add("show");
-      }, index * 200); // 200ms de diferença entre cada item
-  });
-});
 }
