e4103497aaebfda9661fcb73a8f469b0913de4cc
vitorbretz <vitorbretz28@gmail.com>
2025-04-03 23:28:58 -0300

    new

--- src/scripts/modules/scrowProduct.js
+++ src/scripts/modules/scrowProduct.js
@@ -1,52 +1,59 @@
 export default class ScrowProduct {
-    constructor(slide, wrapper){
-      this.slide = document.querySelector(slide);
-      this.wrapper = document.querySelector(wrapper) ;
-      this.dist = {
-        finalPosition: 0,
-        startX: 0,
-        movement: 0
-      } 
-    }
+    constructor(slide, wrapper) {
+        this.slide = slide;
+        this.wrapper = wrapper;
+        this.dist = { finalPosition: 0, startX: 0, movement: 0 };
+        this.bindEvent();
+      }
+
     moveSlide(distX) {
-        this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
-    }
-    updatePostion(clientX){
+    const maxMove = 0; // Define um limite máximo (ex: primeira posição)
+    const minMove = -(this.slide.scrollWidth - this.wrapper.offsetWidth); // Limite mínimo baseado no tamanho do conteúdo
+
+    const newPosition = Math.max(Math.min(distX, maxMove), minMove);
+    this.slide.style.transform = `translate3d(${newPosition}px, 0, 0)`;
+}
+
+    updatePosition(clientX) {
         this.dist.movement = this.dist.startX - clientX;
-        return this.dist.movement;
+        return this.dist.finalPosition - this.dist.movement;
     }
-   
-    onStart(event){
+
+    onStart(event) {
         event.preventDefault();
-        this.dist.startX = event.clientX;
-        
+        this.dist.startX = event.clientX || event.touches[0].clientX;
         this.wrapper.addEventListener('mousemove', this.onMove);
-        
+        this.wrapper.addEventListener('mouseup', this.onEnd);
+        this.wrapper.addEventListener('touchmove', this.onMove);
+        this.wrapper.addEventListener('touchend', this.onEnd);
     }
-    bindEvent(){
+
+    onMove(event) {
+        const clientX = event.clientX || event.touches[0].clientX;
+        const finalPosition = this.updatePosition(clientX);
+        this.moveSlide(finalPosition);
+    }
+
+    onEnd() {
+        this.dist.finalPosition = this.dist.finalPosition - this.dist.movement;
+        this.wrapper.removeEventListener('mousemove', this.onMove);
+        this.wrapper.removeEventListener('mouseup', this.onEnd);
+        this.wrapper.removeEventListener('touchmove', this.onMove);
+        this.wrapper.removeEventListener('touchend', this.onEnd);
+    }
+
+    addSlideEvents() {
+        this.wrapper.addEventListener('mousedown', this.onStart);
+        this.wrapper.addEventListener('touchstart', this.onStart);
+    }
+
+    bindEvent() {
         this.onStart = this.onStart.bind(this);
         this.onMove = this.onMove.bind(this);
-        this.onEnd = this.onEnd.bind(this)
-       }
+        this.onEnd = this.onEnd.bind(this);
+    }
 
-    onMove(event){
-        const finalPosition = this.updatePostion(event.clientX);
-        this.moveSlide(finalPosition)
-        
-    }
-    onEnd(event){
-        this.wrapper.removeEventListener('mousemove', this.onMove)
-        
-    }
-    
-    addSlideEvents(){
-        this.wrapper.addEventListener('mousedown', this.onStart);
-        this.wrapper.addEventListener('mouseup', this.onEnd);
-      
-    }
-   
-    init(){
-        this.bindEvent();
+    init() {
         this.addSlideEvents();
         return this;
     }
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,9 +2,15 @@ import slideShow from './modules/slide.js';
 import menuShow from './pages/header.js';
 import ScrowProduct from  '../scripts/modules/scrowProduct.js';
 
-const scrowProduct = new ScrowProduct('slide-product','.itens-product');
+document.querySelectorAll('.wrapper-product').forEach(wrapper => {
+    const slide = wrapper.querySelector('.slide-product');
+    if (slide) {
+      const scrow = new ScrowProduct(slide, wrapper);
+      scrow.init();
+    }
+  });
 
-scrowProduct.init();
+// scrowProduct.init()
 
 slideShow();
 menuShow();
