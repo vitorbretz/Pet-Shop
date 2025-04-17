c744ed7928b01de591493f0bb0749cf5efe7c6fe
vitorbretz <vitorbretz28@gmail.com>
2025-04-17 01:19:23 -0300

    favicon

---
+++ src/scripts/modules/animationsScrow.js
@@ -0,0 +1,10 @@
+export default function initAnimationScrow(){
+    const sections = document.querySelectorAll('.js-scroll');
+
+    function animaScroll(){
+        console.log('ssim ')
+    }
+
+    window.addEventListener('scroll', animaScroll)
+
+}
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,6 +2,7 @@ import slideShow from './modules/slide.js';
 import menuShow from './pages/header.js';
 import ScrowProduct from  '../scripts/modules/scrowProduct.js';
 import initScrollSuave from './modules/scrollSuave.js';
+import initAnimationScrow from './modules/animationsScrow.js'
 
 
 document.querySelectorAll('.wrapper-product').forEach(wrapper => {
