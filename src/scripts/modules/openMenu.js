18eafafee1106264c905cf2b9c2d172c2c71a4f8
vitorbretz <vitorbretz28@gmail.com>
2025-04-23 00:33:48 -0300

    att

--- src/scripts/modules/openMenu.js
+++ src/scripts/modules/openMenu.js
@@ -2,26 +2,27 @@ export default function openMenu() {
     const abrirBtn = document.querySelector("#open-loguin");
     const fecharBtn = document.querySelector(".x-btn");
     const loguin = document.querySelector(".conteiner-loguin");
+    
   
     if (!abrirBtn || !fecharBtn || !loguin) return;
 
-    // Abrir o container de login
+    
     abrirBtn.addEventListener("click", () => {
         loguin.classList.add("ativo");
-        loguin.style.opacity = '1'; // Assegura que a opacidade esteja visível ao abrir
+        loguin.style.opacity = '1'; 
     });
   
-    // Fechar o container de login
+    
     fecharBtn.addEventListener("click", () => {
         loguin.classList.remove("ativo");
-        loguin.style.opacity = '0'; // Opcional: pode adicionar uma transição de opacidade se desejar
+        loguin.style.opacity = '0'; 
     });
 
-    // Fechar o container ao clicar fora
+    
     window.addEventListener('click', (event) => {
         if (!loguin.contains(event.target) && !abrirBtn.contains(event.target)) {
             loguin.classList.remove("ativo");
-            loguin.style.opacity = '0'; // Opacidade zero quando fechado
+            loguin.style.opacity = '0'; 
         }
     });
 }
